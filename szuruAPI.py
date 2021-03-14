ecfc3012bf02e6a6db3e59e0c888964c8a8633b0
Ferns <jeffreyzhang123@gmail.com>
2021-03-13 19:14:03 -0800

    Tagger works

--- szuruAPI.py
+++ szuruAPI.py
@@ -3,6 +3,7 @@ import json
 
 config = json.load(open("config.json"))
 
+# Returns a search result resource as a python dict
 def getPosts(query = "", count = 0, offset = 0):
 	if(count <= 0):
 		count = ""
@@ -10,6 +11,8 @@ def getPosts(query = "", count = 0, offset = 0):
 	posts = requests.get(url, headers = {"Accept": "application/json"}, auth=requests.auth.HTTPBasicAuth(config["user"], config["password"]))
 	return posts.json()
 
+# Takes in the post id as a number and the post update object as a dict
+# Returns the status given
 def updatePost(id, post):
 	url = config["url"] + "/api/post/" + str(id)
 	header = {"Accept": "application/json", "Content-Type": "application/json"}
@@ -19,3 +22,8 @@ def createComment(comment):
 	url = config["url"] + "/api/comments/"
 	header = {"Accept": "application/json", "Content-Type": "application/json"}
 	return requests.post(url, headers = header, data = json.dumps(comment), auth=requests.auth.HTTPBasicAuth(config["user"], config["password"]))
+
+# Takes a post resource and returns the response
+def getThumbnail(post):
+	url = config["url"] + "/" + post["thumbnailUrl"]
+	return requests.get(url)
--- szurutagger.py
+++ szurutagger.py
@@ -1,2 +1,23 @@
 import deepdanbooru as dd
+import szuruAPI as s
+import io
 
+model = dd.project.load_model_from_project("pretrained", compile_model = False)
+
+# Needs a project folder under the folder "pretrained"
+tags = dd.project.load_tags_from_project("pretrained")
+
+posts = s.getPosts("-tag:bot_tagged,mod_confirmed", 0)["results"]
+for x in posts:
+	oldTags = ["bot_tagged"]
+	for category in x["tags"]:
+		for tag in category["names"]:
+			oldTags.append(tag)
+	thumb = s.getThumbnail(x).content
+	d = dd.commands.evaluate_image(io.BytesIO(thumb), model, tags, 0.5)
+	comment = ""
+	postUpdate = { "version": x["version"], "tags": oldTags }
+	for tup in d:
+		postUpdate["tags"].append(tup[0])
+		comment += f"({tup[1]:05.3f}) {tup[0]}\n"
+	s.updatePost(x["id"], postUpdate)
